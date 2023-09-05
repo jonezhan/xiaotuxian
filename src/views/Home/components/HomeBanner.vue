9fce6fcd661396cd21502de0f2ab6a6a45780b33
zhan <1748965108@qq.com>
2023-09-05 21:06:49 +0800

    Home-banner轮播图实现

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,5 +1,37 @@
-<script setup></script>
+<script setup>
+import { getBannerAPI } from "@/apis/home";
+import { ref, onMounted } from "vue";
+
+const bannerList = ref([]);
+const getbanner = async () => {
+  const res = await getBannerAPI();
+  bannerList.value = res.result;
+};
+onMounted(() => getbanner());
+</script>
 
 <template>
-  <div>HomeBanner</div>
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item">
+        <img :src="item.imgUrl" alt="" />
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
