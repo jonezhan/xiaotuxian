54e8a224db99dbc4c660e15c95068bb66cc4d5d5
zhan <1748965108@qq.com>
2023-09-06 17:18:31 +0800

    二级分类-面包屑导航实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,4 +1,24 @@
-<script setup></script>
+<script setup>
+import { getCategoryFilterAPI } from "@/apis/category";
+import { onBeforeRouteUpdate, useRoute } from "vue-router";
+import { onMounted, ref } from "vue";
+
+// 获取分类数据
+const categoryData = ref({});
+const route = useRoute();
+const getCategoryData = async (id = route.params.id) => {
+  const res = await getCategoryFilterAPI(id);
+  categoryData.value = res.result;
+};
+onMounted(() => getCategoryData());
+
+// <!-- 解决路由缓存问题2 -->
+// 目标：路由参数变化时 可以把分类数据接口重新发送
+// onBeforeRouteUpdate((to) => {
+// 应该使用最新的路由参数请求最新的分类数据 通过to获得相关数据
+//   getCategory(to.params.id);
+// });
+</script>
 
 <template>
   <div class="container">
@@ -6,8 +26,10 @@
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家 </el-breadcrumb-item>
-        <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+        <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }"
+          >{{ categoryData.parentName }}
+        </el-breadcrumb-item>
+        <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <div class="sub-container">
