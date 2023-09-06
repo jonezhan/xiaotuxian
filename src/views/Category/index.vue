b3d14d554bba81c2a348f67825fe0a78511222c3
zhan <1748965108@qq.com>
2023-09-06 14:39:45 +0800

    一级分类-轮播图功能实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { getCategoryAPI } from "@/apis/category";
+import { getBannerAPI } from "@/apis/home";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 
@@ -11,6 +12,16 @@ const getCategory = async () => {
   categoryData.value = res.result;
 };
 onMounted(() => getCategory());
+
+// 获取轮播图数据
+const bannerList = ref([]);
+const getbanner = async () => {
+  const res = await getBannerAPI({
+    distributionSite: "2",
+  });
+  bannerList.value = res.result;
+};
+onMounted(() => getbanner());
 </script>
 
 <template>
@@ -23,11 +34,33 @@ onMounted(() => getCategory());
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item">
+            <img :src="item.imgUrl" alt="" />
+          </el-carousel-item>
+        </el-carousel>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  // position: absolute;
+  // left: 0;
+  // top: 0;
+  // z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+
 .top-category {
   h3 {
     font-size: 28px;
