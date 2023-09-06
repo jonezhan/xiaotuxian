910c15b945ecca18e686ecbf6bcadc0e77894004
zhan <1748965108@qq.com>
2023-09-06 20:39:24 +0800

    详情页-整体认识和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Layout from "@/views/Layout/index.vue";
 import Home from "@/views/Home/index.vue";
 import Category from "@/views/Category/index.vue";
 import SubCategory from "@/views/SubCategory/index.vue";
+import Detail from "@/views/Detail/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,7 @@ const router = createRouter({
         },
         { path: "category/:id", component: Category },
         { path: "category/sub/:id", component: SubCategory },
+        { path: "detail/:id", component: Detail },
       ],
     },
     {
