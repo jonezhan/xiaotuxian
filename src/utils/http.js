72115c939772d2b5a6388d277eaf1f8434e97b27
zhan <1748965108@qq.com>
2023-09-09 20:43:21 +0800

    登录-请求拦截器携带Token

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { ElMessage, messageConfig } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
 
+import { useUserStore } from "@/stores/user";
+
 // 创建axios实例
 const httpInstance = axios.create({
   baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
@@ -11,6 +13,15 @@ const httpInstance = axios.create({
 // axios请求拦截器
 httpInstance.interceptors.request.use(
   (config) => {
+    // 从pinia获取token数据
+    const userStore = useUserStore();
+
+    // 按照后端的要求拼接token数据
+    const token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
     return config;
   },
   (e) => Promise.reject(e)
