0f2c8f4d6c10ade21eba1641fee83572db6bad86
zhan <1748965108@qq.com>
2023-09-10 20:29:15 +0800

    购物车-本地-购物车列表统计数据实现

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -58,6 +58,20 @@ export const useCartStore = defineStore(
       cartList.value.reduce((a, c) => a + c.count * c.price, 0)
     );
 
+    // 3.已选择的数量
+    const selectedCount = computed(() =>
+      cartList.value
+        .filter((item) => item.selected)
+        .reduce((a, c) => a + c.count, 0)
+    );
+
+    // 4.已选择的商品价格合计
+    const selectedPrice = computed(() =>
+      cartList.value
+        .filter((item) => item.selected)
+        .reduce((a, c) => a + c.count * c.price, 0)
+    );
+
     // 是否全选
     const isAll = computed(() => cartList.value.every((item) => item.selected));
 
@@ -70,6 +84,8 @@ export const useCartStore = defineStore(
       singleCheck,
       isAll,
       allCheck,
+      selectedCount,
+      selectedPrice,
     };
   },
   {
