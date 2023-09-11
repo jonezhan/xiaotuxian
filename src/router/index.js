1be789f93918e8592a0760e029f8f86c083e3081
zhan <1748965108@qq.com>
2023-09-11 18:08:11 +0800

    结算-生成订单功能实现

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import SubCategory from "@/views/SubCategory/index.vue";
 import Detail from "@/views/Detail/index.vue";
 import CartList from "@/views/CartList/index.vue";
 import Checkout from "@/views/Checkout/index.vue";
+import Pay from "@/views/Pay/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,7 @@ const router = createRouter({
         { path: "detail/:id", component: Detail },
         { path: "cartlist", component: CartList },
         { path: "checkout", component: Checkout },
+        { path: "pay", component: Pay },
       ],
     },
     {
