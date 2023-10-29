18f4c92f6cad736725afbf6f76d177a0c1546963
zhan <1748965108@qq.com>
2023-10-29 20:49:55 +0800

    会员中心点击跳转

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -40,7 +40,12 @@ const confirm = () => {
             </el-popconfirm>
           </li>
           <li><a href="javascript:;">我的订单</a></li>
-          <li><a href="javascript:;">会员中心</a></li>
+
+          <li>
+            <a href="javascript:;">
+              <RouterLink to="/member">会员中心</RouterLink>
+            </a>
+          </li>
         </template>
         <template v-else>
           <li>
